1c3957daba64db7add8fe212dd1b6c07d8e9888f
Arnaud Lahaut <arnaudlht@gmail.com>
2025-03-28 19:36:03 +0100

    feat: implement custom font system with Poppins, Bebas Neue and Necto Mono

---
+++ src/assets/fonts/fonts.ts
@@ -0,0 +1,19 @@
+import { Poppins, Bebas_Neue } from "next/font/google";
+import localFont from "next/font/local";
+
+export const poppins = Poppins({
+	subsets: ["latin"],
+	weight: ["400", "500", "600", "700"],
+	variable: "--font-poppins",
+});
+
+export const bebasNeue = Bebas_Neue({
+	subsets: ["latin"],
+	weight: ["400"],
+	variable: "--font-bebas-neue",
+});
+
+export const nectoMono = localFont({
+	src: "./NectoMono-Regular.woff",
+	variable: "--font-necto-mono",
+});
