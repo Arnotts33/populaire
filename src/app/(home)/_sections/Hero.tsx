0d2bc78349851a1b99c32cc9adf60bd079b30642
Arnaud Lahaut <arnaudlht@gmail.com>
2025-03-29 11:43:27 +0100

    fix: hero image responsiveness on large screens

--- src/app/(home)/_sections/Hero.tsx
+++ src/app/(home)/_sections/Hero.tsx
@@ -5,7 +5,12 @@ import styles from "./Hero.module.css";
 const Hero = () => {
 	return (
 		<section className={styles.hero}>
-			<Image src={heroImg} alt="Kitchen" className={styles.heroImg} />
+			<Image
+				src={heroImg}
+				alt="Kitchen"
+				className={styles.heroImg}
+				priority={true}
+			/>
 			<div className={styles.heroText}>
 				<h1>Dwitcherie</h1>
 				<h2>Bar à Manger, Cave à vins</h2>
