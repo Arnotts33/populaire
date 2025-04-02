b57cbadbaa6153e9f9e971bf7f9203e52c24647c
Arnaud Lahaut <arnaudlht@gmail.com>
2025-04-02 18:46:55 +0200

    feat: implement responsive grid layout for dwitches section with positioned elements

--- src/app/(home)/_sections/Dwitches.tsx
+++ src/app/(home)/_sections/Dwitches.tsx
@@ -2,19 +2,19 @@ import styles from "./Dwitches.module.css";
 import sandwich from "@/assets/images/grilled-cheese.webp";
 import daftpunk from "@/assets/images/daftpunk.webp";
 import Image from "next/image";
+import ScrollingKeywords from "@/components/ui/ScrollingKeywords";
 
 const Dwitches = () => {
 	return (
 		<section className={styles.dwitchesSection} id="dwitches">
+			<div className={styles.keywordsChain}>
+				<ScrollingKeywords />
+				<ScrollingKeywords />
+			</div>
+
 			<div className={styles.dwitchesContent}>
 				<div className={styles.title}>
 					<h1 className={styles.titleText}>Dwitches</h1>
-				</div>
-				<div className={styles.keywordsChain}>
-					<p>
-						grilled cheese • smash • tacos • bao • baguette • grilled cheese
-					</p>
-					<p>baguette • grilled cheese • smash • tacos • bao • baguette</p>
 				</div>
 				<div className={styles.albumCover}>
 					<Image
