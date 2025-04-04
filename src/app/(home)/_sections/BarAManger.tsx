9d2a0ef1cf452db84f5c78ed7a9955199395e2ba
Arnaud Lahaut <arnaudlht@gmail.com>
2025-04-04 22:21:22 +0200

    feat: implement responsive bar à manger section with adaptive typography and layout

--- src/app/(home)/_sections/BarAManger.tsx
+++ src/app/(home)/_sections/BarAManger.tsx
@@ -1,9 +1,41 @@
 import styles from "./BarAManger.module.css";
+import Image from "next/image";
+import table from "@/assets/images/table.webp";
+import doomsday from "@/assets/images/doomsday.webp";
 
 const BarAManger = () => {
 	return (
-		<section className={styles.barAManger} id="bar-a-manger">
-			<h2>Bar à Manger Section</h2>
+		<section className={styles.barAMangerSection} id="bar-a-manger">
+			<div className={styles.barAMangerContent}>
+				<div className={styles.topContent}>
+					<div className={styles.imageContainer}>
+						<Image
+							src={table}
+							alt="Table de restaurant"
+							className={styles.barAMangerImage}
+						/>
+					</div>
+					<div className={styles.titleContainer}>
+						<div className={styles.album}>
+							<Image
+								src={doomsday}
+								alt="Doomsday album cover"
+								className={styles.albumCoverimage}
+							/>
+						</div>
+						<div className={styles.title}>
+							<h1 className={styles.titleText}>Bar à Manger</h1>
+						</div>
+					</div>
+				</div>
+				<div className={styles.bottomContent}>
+					<p className={styles.descriptionText}>
+						Ici, on partage des assiettes bien senties et des vins qui claquent.
+						Du bon produit, du goût, une cuisine qui va à l’essentiel. Un bar à
+						manger où tu viens grignoter, trinquer, traîner.
+					</p>
+				</div>
+			</div>
 		</section>
 	);
 };
