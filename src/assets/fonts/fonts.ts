0f5ab6aced14834e6d14adba3d8299deb3c8f458
Arnaud Lahaut <arnaudlht@gmail.com>
2025-04-08 11:00:22 +0200

    test: hero title on ios

--- src/assets/fonts/fonts.ts
+++ src/assets/fonts/fonts.ts
@@ -1,4 +1,4 @@
-import { Poppins, Abril_Fatface } from "next/font/google";
+import { Poppins, Bebas_Neue, Abril_Fatface } from "next/font/google";
 import localFont from "next/font/local";
 
 export const poppins = Poppins({
@@ -7,10 +7,10 @@ export const poppins = Poppins({
 	variable: "--font-poppins",
 });
 
-export const bebasNeue = localFont({
-	src: "../fonts/BebasNeue.woff2",
+export const bebasNeue = Bebas_Neue({
+	subsets: ["latin"],
+	weight: ["400"],
 	variable: "--font-bebas-neue",
-	display: "swap",
 });
 
 export const nectoMono = localFont({
