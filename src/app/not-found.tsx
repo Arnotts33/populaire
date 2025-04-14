c3f4b7bc49b94c25e33f70e56afdb9fab1d40a61
Arnaud Lahaut <arnaudlht@gmail.com>
2025-04-14 15:49:16 +0200

    feat: page not found done and responsive

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,29 @@
+import Image from "next/image";
 import styles from "./not-found.module.css";
+import panier from "@/assets/images/panier-hotdog.webp";
+import Link from "next/link";
 
 const NotFound = () => {
 	return (
-		<div className={styles.notFound}>
-			<h1 className={styles.notFoundText}>404 - Page Not Found</h1>
-		</div>
+		<main className={styles.notFound}>
+			<div className={styles.notFoundContainer}>
+				<h1 className={styles.leftFour}>4</h1>
+				<Image
+					className={styles.notFoundImage}
+					src={panier}
+					alt="Panier à hotdog vide"
+				/>
+				<h1 className={styles.rightFour}>4</h1>
+			</div>
+			<div className={styles.notFoundTextContainer}>
+				<p className={styles.notFoundText}>
+					Oups... Plus rien à manger par ici!
+				</p>
+				<Link href={"/"} className={styles.homeLink}>
+					Retour à l'accueil
+				</Link>
+			</div>
+		</main>
 	);
 };
 export default NotFound;
