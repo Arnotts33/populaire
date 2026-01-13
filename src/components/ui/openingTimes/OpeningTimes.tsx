7068ab923ea69b2928b6447c97fb6798dc2fd859
Arnaud Lahaut <arnaudlht@gmail.com>
2026-01-13 11:01:58 +0100

    add: new opening hours

--- src/components/ui/openingTimes/OpeningTimes.tsx
+++ src/components/ui/openingTimes/OpeningTimes.tsx
@@ -4,9 +4,10 @@ const OpeningTimes = () => {
 	return (
 		<div className={styles.openingTimes}>
 			<h2 className={styles.title}>Horaires d&apos;ouverture :</h2>
-			<p className={styles.text}>Mercredi - Samedi : 12h - 19h30</p>
-			<p className={styles.text}>Jeudi - Vendredi : 12h - 00h</p>
-			<p className={styles.text}>Dimanche : 11h - 16h</p>
+			<p className={styles.text}>
+				Jeudi - Samedi : 12h - 15h30 / 18h30 - minuit
+			</p>
+			<p className={styles.text}>Dimanche : 12h - 16h</p>
 		</div>
 	);
 };
